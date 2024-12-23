8c1df1f5597a65ce9a7388b1d9cd2540b80fe773
RakibulBh <rakibulbhuiyan1973@gmail.com>
2024-12-23 15:15:26 +0000

    add: reset full rpoject

--- app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <div className="bg-tertiary flex justify-center items-center flex-col gap-10 w-full h-screen">
-      <SignIn />
-    </div>
-  );
-}
--- app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <div className="bg-tertiary flex justify-center items-center flex-col gap-10 w-full h-screen">
-      <SignUp />
-    </div>
-  );
-}
